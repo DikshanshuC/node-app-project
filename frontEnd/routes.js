15956f894722b46b326b035d071135805052cf3c
DikshanshuC <165465768+DikshanshuC@users.noreply.github.com>
2025-03-20 14:29:45 +0530

    Update routes.js

--- frontEnd/routes.js
+++ frontEnd/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fetch = require('node-fetch')
 const router = express.Router()
 
-const host= 'http://3.111.171.251:8000/'
+const host= 'http://13.126.226.170:8000/'
 
 router.get('/', (req, res)=>{
     res.render('index')
