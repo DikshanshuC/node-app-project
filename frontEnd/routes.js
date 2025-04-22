2e13035453d138f7fec269bea782f78881cb3e61
DikshanshuC <165465768+DikshanshuC@users.noreply.github.com>
2025-04-22 10:58:58 +0530

    Update routes.js

--- frontEnd/routes.js
+++ frontEnd/routes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const fetch = require('node-fetch')
 const router = express.Router()
 
-const host= 'http://backend:8000'
+const host= 'http://backend-service:8000'  // Use service name
+
 
 router.get('/', (req, res)=>{
     res.render('index')
